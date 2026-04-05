1336003661b10b94282d69cd76a91f4cb1821e8e
heeeete <mkoui@naver.com>
2026-04-05 23:02:44 +0900

    feat: Vercel Analytics 추가

--- src/app/layouts/layout.tsx
+++ src/app/layouts/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Script from 'next/script';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,7 +36,9 @@ export default function RootLayout({
             strategy="beforeInteractive"
           />
         )}
+
         <main className="flex-1">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
